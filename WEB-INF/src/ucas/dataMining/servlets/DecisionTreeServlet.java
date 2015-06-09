60d3ef932363e7846857b5c5fa957b300833f5e1
JeffChan <nieyaoyanJune@gmail.com>
2015-06-09 11:22:10 +0800

    决策树增加交叉测试
    决策树增加交叉测试，以及上传、训练、测试分离。

--- WEB-INF/src/ucas/dataMining/servlets/DecisionTreeServlet.java
+++ WEB-INF/src/ucas/dataMining/servlets/DecisionTreeServlet.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -25,32 +27,62 @@ import ucas.dataMining.util.FileIOUtil;
 @WebServlet("/decisionTreeServlet")
 public class DecisionTreeServlet extends HttpServlet {
 	private static final long serialVersionUID = 7365238949032150065L;
-	private DecisionTree decisionTree;
-	private String classificationName = "";
+	private String[] columnNames;  //属性名
+	private List<String[]> instances;  //训练实例
+	private DecisionTree decisionTree;  //决策树
+	private String testResult;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException ,IOException {
+		String actionType = request.getParameter("type");  //请求处理类型：测试、训练、预测
+		
 		String responseMessage = "";
 		
-		//结果预测
-		if(decisionTree == null) {
-			responseMessage = "没有可用决策树";
-		}
-		else {
-			Map<String, String> caseMap = new HashMap<String, String>();
-			
-			LinkedHashSet<String> attributeNames = decisionTree.getAttributeNames();
-			Iterator<String> iterator = attributeNames.iterator();
-			while(iterator.hasNext()) {
-				String attributeName = iterator.next();
-				caseMap.put(attributeName, request.getParameter(attributeName));
-				System.out.println(attributeName + ": " + request.getParameter(attributeName));
+		if(actionType.equals("training")) {
+			try {
+				decisionTreeMaker();
+				responseMessage = columnNames[columnNames.length - 1];
+				responseMessage += decisionTree.getAttributeNames().toString();
+			}
+			catch(Exception ex) {
+				responseMessage = "Error: " + ex.getMessage();
+				System.out.println(responseMessage);
 			}
 			
-			try {
-				responseMessage = decisionTree.classify(caseMap);
-			} catch (UnknownDecisionException e) {
-				responseMessage = "UNKNOWN";
+			System.out.println("决策树训练完毕！");
+		}
+		else if(actionType.equals("test")) {
+			if(testResult != null) {
+				responseMessage = testResult;
+			}
+			else {
+				responseMessage = "Error: 没有训练集!";
+			}
+			System.out.println("决策树测试完毕！结果： " + responseMessage);
+		}
+		else {
+			// 结果预测
+			if (decisionTree == null) {
+				responseMessage = "没有可用决策树";
+			} 
+			else {
+				Map<String, String> caseMap = new HashMap<String, String>();
+
+				LinkedHashSet<String> attributeNames = decisionTree.getAttributeNames();
+				Iterator<String> iterator = attributeNames.iterator();
+				while (iterator.hasNext()) {
+					String attributeName = iterator.next();
+					caseMap.put(attributeName, request.getParameter(attributeName));
+				}
+
+				try {
+					responseMessage = decisionTree.classify(caseMap);
+					
+				} catch (UnknownDecisionException e) {
+					responseMessage = "UNKNOWN";
+				}
+				
+				System.out.println("实例预测完成！结果：" + responseMessage);
 			}
 		}
 		
@@ -63,29 +95,28 @@ public class DecisionTreeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Part file = request.getPart("file");
         //String filename = file.getSubmittedFileName();
-        
-        String responseMessage;
+        System.out.println("决策树数据集上传成功！");
+        String responseMessage = "";
         //处理传入的文件
         try{
-        	decisionTreeMaker(file.getInputStream());
-        	responseMessage = classificationName;
-        	responseMessage += decisionTree.getAttributeNames().toString();
-        } 
+        	//FileIOUtil.saveToLocal(this.getServletContext().getRealPath("/uploadFile/decisionTree.csv"), file.getInputStream());
+        	loadTrainingSet(file.getInputStream());
+        }
         catch(Exception ex) {
         	responseMessage = "Error: " + ex.getMessage();
         }
-        
-        System.out.println(responseMessage);
+
         response.setContentType("text/plain");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(responseMessage);
     }
 	
 	/**
-	 * 决策树生成
+	 * 数据预处理
+	 * @throws IOException 
 	 */
-	private void decisionTreeMaker(InputStream dataSet) throws IOException {
-		decisionTree = new DecisionTree();
+	private void loadTrainingSet(InputStream dataSet) throws IOException {
+		decisionTree = null;
 		
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataSet));
 		
@@ -94,25 +125,86 @@ public class DecisionTreeServlet extends HttpServlet {
 		if(line == null) {		
 			return;
 		}
-		String[] columnNames = line.trim().split(",");
-		classificationName = columnNames[columnNames.length - 1];
-		decisionTree.setAttributeNames(arrayCut(columnNames));
 		
+		columnNames = line.trim().split(",");		
+		instances = new ArrayList<String[]>();
 		//添加训练实例
 		line = bufferedReader.readLine();
 		while(line != null) {
-			String[] instance = line.trim().split(",");
-			decisionTree.addInstance(arrayCut(instance), instance[instance.length - 1]);
+			instances.add(line.trim().split(","));
 			line = bufferedReader.readLine();
 		}
 		
 		dataSet.close();
 		bufferedReader.close();
+	}
+	
+	/**
+	 * 决策树生成
+	 */
+	private void decisionTreeMaker() throws Exception {
+		if(decisionTree != null ) {
+			return;
+		}
+		
+		testResult = null;
+		
+		decisionTree = new DecisionTree();		
+		if(instances.size() < 1) {
+			throw new Exception("没有训练集!");
+		}
+		
+		decisionTree.setAttributeNames(arrayCut(columnNames));
+		
+		for(String[] instance : instances) {
+			decisionTree.addInstance(arrayCut(instance), instance[instance.length - 1]);
+		}
 		
 		//生成结果
-		FileIOUtil.writeToFile(decisionTree.toJson().toJSONString(),
-				this.getServletContext().getRealPath("/") + "\\json\\decision_tree.json"
-				);
+		FileIOUtil.writeToFile(decisionTree.toJson().toJSONString(), this.getServletContext().getRealPath("/json/decision_tree.json"));
+		
+		testResult = crossValidation();
+	}
+	
+	/**
+	 * 算法交叉验证 leave-one-out
+	 * @return
+	 */
+	private String crossValidation() {
+		if(instances.size() < 1) {
+			return "没有训练集!";
+		}
+		
+		int totalSize = instances.size();  //训练集大小
+		int correctCount = 0;   //预测正确实例数
+		
+		for(int i = 0; i < totalSize; i++) {
+			//实例训练
+			DecisionTree dTree = new DecisionTree();
+			dTree.setAttributeNames(arrayCut(columnNames));
+			for(int j = 0; j < totalSize; j++) {
+				if(j != i) {
+					String[] instance = instances.get(j);
+					dTree.addInstance(arrayCut(instance), instance[instance.length - 1]);
+				}
+			}
+			
+			Map<String, String> testInstance = new HashMap<String, String>();
+			for(int k = 1; k < columnNames.length - 1; k++) {
+				testInstance.put(columnNames[k], instances.get(i)[k]);
+			}
+			
+			//测试
+			try {
+				if(dTree.classify(testInstance).equals(instances.get(i)[columnNames.length - 1])) {
+					correctCount++;
+				}
+			} catch (UnknownDecisionException e) {
+				
+			}
+		}
+		
+		return "数据样例：" + totalSize + "    错误样例：" + (totalSize - correctCount) + "    正确率：" + (((double)correctCount) / totalSize) * 100 + "%";
 	}
 	
 	private String[] arrayCut(String[] strings) {
